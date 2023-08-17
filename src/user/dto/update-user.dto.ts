715db2c038c89aa3549ca746ba08506c7f07e14e
Mohsin I Amin <mohsinamin953@gmail.com>
2023-08-17 19:07:51 +0600

    validation and swagger

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,9 +1,38 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import {
+    IsEmail,
+    IsString,
+    MinLength,
+    MaxLength
+} from 'class-validator';
+
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
+    @ApiPropertyOptional({
+        type: String,
+    })
+    @IsEmail({}, { message: 'Please provide valid Email.' })
     email: string;
+
+    @ApiPropertyOptional({
+        type: String,
+    })
+    @IsString()
+    @MaxLength(11, { message: "Please provide a valid phone number." })
+    @MinLength(11, { message: "Please provide a valid phone number." })
     contactNo: string;
+
+    @ApiPropertyOptional({
+        type: String,
+    })
+    @IsString()
     password: string;
+
+    @ApiPropertyOptional({
+        type: String,
+    })
+    @IsString()
     profilePic: string;
 }
