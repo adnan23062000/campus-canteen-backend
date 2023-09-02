a3126ecd15356de0a99a36ae4b10c64406bb9677
adnan23062000 <bsse1131@iit.du.ac.bd>
2023-09-03 00:27:49 +0600

    user-crud/fix: make some validation fields optional

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,79 +1,69 @@
-
-import {
-    IsEmail,
-    IsString,
-    MinLength,
-    MaxLength, IsNotEmpty, IsInt, IsEnum
-} from 'class-validator';
-
+import { IsEmail, IsString, MinLength, MaxLength, IsNotEmpty, IsInt, IsEnum, IsOptional } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 import { UserRole } from '../entities/user.entity';
+  
 export class CreateUserDto {
-
+  
     @ApiProperty({
-        type: String,
-        description: 'fullname is required',
+      type: String,
+      description: 'fullname is required',
     })
     @IsString()
-    @MinLength(2, { message: 'Name must have atleast 2 characters.' })
+    @MinLength(2, { message: 'Name must have at least 2 characters.' })
     @IsNotEmpty()
     readonly fullname: string;
-
+  
     @ApiProperty({
-        type: String,
-        description: 'email is required',
+      type: String,
+      description: 'email is required',
     })
     @IsNotEmpty()
-    @IsEmail({}, { message: 'Please provide valid Email.' })
+    @IsEmail({}, { message: 'Please provide a valid Email.' })
     readonly email: string;
-
+  
     @ApiProperty({
-        type: String,
-        description: 'contactNo is required',
+      type: String,
+      description: 'contactNo is required',
     })
     @IsNotEmpty()
     @IsString()
     @MaxLength(11, { message: "Please provide a valid phone number." })
     @MinLength(11, { message: "Please provide a valid phone number." })
     readonly contactNo: string;
-
+  
     @ApiProperty({
-        type: String,
-        description: 'password is required',
+      type: String,
+      description: 'password is required',
     })
-    // @Matches(passwordRegEx, {
-    //     message: `Password must contain Minimum 8 and maximum 20 characters, 
-    // at least one uppercase letter, 
-    // one lowercase letter, 
-    // one number and 
-    // one special character`,
-    // })
     @IsNotEmpty()
     password: string;
-
+  
     @ApiProperty({
-        type: Number,
-        description: 'registrationNumber is required',
+      type: Number,
+      description: 'registrationNumber is optional', 
     })
+    @IsOptional() 
     @IsInt()
-    @IsNotEmpty()
-    registrationNumber: number;
-
+    registrationNumber?: number; 
+  
     @ApiProperty({
-        type: String,
-        description: 'profilePic is required',
+      type: String,
+      description: 'profilePic is optional', 
     })
+    @IsOptional() 
     @IsString()
-    profilePic: string;
-
+    profilePic?: string; 
+  
     @ApiProperty({
-        type: String,
-        description: 'idCard is required',
+      type: String,
+      description: 'idCard is optional', 
     })
+    @IsOptional() 
     @IsString()
-    idCard: string;
-
+    idCard?: string; 
+  
     @ApiProperty({ enum: UserRole })
     @IsEnum(UserRole, { message: 'Invalid role value' })
     role: UserRole;
 }
+  
--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,38 +1,33 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
-import {
-    IsEmail,
-    IsString,
-    MinLength,
-    MaxLength
-} from 'class-validator';
-
+import { IsEmail, IsString, MinLength, MaxLength, IsOptional } from 'class-validator';
 import { ApiPropertyOptional } from '@nestjs/swagger';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
-    @ApiPropertyOptional({
-        type: String,
-    })
-    @IsEmail({}, { message: 'Please provide valid Email.' })
-    email: string;
+  @ApiPropertyOptional({
+    type: String,
+  })
+  @IsEmail({}, { message: 'Please provide valid Email.' })
+  email: string;
 
-    @ApiPropertyOptional({
-        type: String,
-    })
-    @IsString()
-    @MaxLength(11, { message: "Please provide a valid phone number." })
-    @MinLength(11, { message: "Please provide a valid phone number." })
-    contactNo: string;
+  @ApiPropertyOptional({
+    type: String,
+  })
+  @IsString()
+  @MaxLength(11, { message: "Please provide a valid phone number." })
+  @MinLength(11, { message: "Please provide a valid phone number." })
+  contactNo: string;
 
-    @ApiPropertyOptional({
-        type: String,
-    })
-    @IsString()
-    password: string;
+  @ApiPropertyOptional({
+    type: String,
+  })
+  @IsString()
+  password: string;
 
-    @ApiPropertyOptional({
-        type: String,
-    })
-    @IsString()
-    profilePic: string;
+  @ApiPropertyOptional({
+    type: String,
+  })
+  @IsOptional() 
+  @IsString()
+  profilePic?: string; 
 }
